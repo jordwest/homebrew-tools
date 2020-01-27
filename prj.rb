95b7dd50d1bc14c7e8d2c0d49d54df4ce5361a9d
Jordan West <jordwest@gmail.com>
2020-01-27 18:03:38 +1100

    fix sha

--- prj.rb
+++ prj.rb
@@ -2,7 +2,7 @@ class Prj < Formula
   desc "Navigate between local repos quickly with fuzzy find and git summaries"
   homepage "https://github.com/jordwest/prj"
   url "https://github.com/jordwest/prj/releases/download/0.1.0/prj-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "980a74aa023441a5220d4d65b8d7b750f098aa840a1c9aa4c86f2c3ea58edbea"
+  sha256 "4f57c1f3ecc4212751e008554c54ef0ba42bf25d1313e861010f1a87bb9dd9a4"
 
   bottle :unneeded
 
